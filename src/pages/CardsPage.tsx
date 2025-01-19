e51ddda554262aae9f39bdbd987fb511a7434ed2
Aleksander Afanasyev <afonia.marcii@gmail.com>
2025-01-19 15:09:52 +0100

    feat:  header and routing for cards and promo pages; update styles and dependencies

---
+++ src/pages/CardsPage.tsx
@@ -0,0 +1,10 @@
+function CardsPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Cards Page</h1>
+      <p>Cards page</p>
+    </div>
+  )
+}
+
+export default CardsPage
---
+++ src/pages/PromoPage.tsx
@@ -0,0 +1,10 @@
+function PromoPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Promo Page</h1>
+      <p>Promo page</p>
+    </div>
+  )
+}
+
+export default PromoPage
